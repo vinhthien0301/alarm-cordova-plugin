bada1d4d7b33e8e4d19fb6a7312f3116920783ed
ThienAu <vinh.thien0301@gmail.com>
2016-02-25 11:58:23 +0700

    fix

--- www/alarm.js
+++ www/alarm.js
@@ -1,7 +1,15 @@
 var Alarm = function() {};
 
+Alarm.prototype.startService = function(success, fail) {
+    cordova.exec(success, fail, "AlarmPlugin","startService", []);
+};
+
+Alarm.prototype.stopService = function(success, fail) {
+    cordova.exec(success, fail, "AlarmPlugin","stopService", []);
+};
+
 Alarm.prototype.wakeup = function(interval, timeFrom, timeTo, success, fail) {
-    cordova.exec(success, fail, "AlarmPlugin","wakeup", [interval, timeFrom, timeTo]);
+    cordova.exec(success, fail, "AlarmPlugin","schedule", [interval, timeFrom, timeTo]);
 };
 
 var alarm = new Alarm();
