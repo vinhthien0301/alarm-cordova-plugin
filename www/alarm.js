83e16fc4fc822fe83bb66401e1367ef537d729f7
ThienAu <vinh.thien0301@gmail.com>
2016-02-29 07:48:14 +0700

    fix

--- www/alarm.js
+++ www/alarm.js
@@ -1,7 +1,7 @@
 var Alarm = function() {};
 
-Alarm.prototype.schedule = function(interval, timeFrom, timeTo, mp3, success, fail) {
-    cordova.exec(success, fail, "AlarmPlugin","schedule", [interval, timeFrom, timeTo, mp3]);
+Alarm.prototype.schedule = function(interval, timeFrom, timeTo, mp3, textMessage, success, fail) {
+    cordova.exec(success, fail, "AlarmPlugin","schedule", [interval, timeFrom, timeTo, mp3, textMessage]);
 };
 
 var alarm = new Alarm();
